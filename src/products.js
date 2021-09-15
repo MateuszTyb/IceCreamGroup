b50636833bf8e481ac553a7c32d29592be6d676f
michal-wolski <michal.wolski130@gmail.com>
2021-09-15 23:08:07 +0200

    Overlay w sekcji produk
    Sprawdz czy sie strona nie posypie i czy JS dziala bo u mnie nie dziala jak mam w oddzielnym pliku

---
+++ src/products.js
@@ -0,0 +1,33 @@
+function on1() {
+  document.getElementById('overlay1').style.display = 'block';
+  document.getElementById('on1').style.display = 'none';
+  document.getElementById('off1').style.display = 'block';
+}
+
+function off1() {
+  document.getElementById('overlay1').style.display = 'none';
+  document.getElementById('on').style.display = 'block';
+  document.getElementById('off').style.display = 'none';
+}
+function on2() {
+  document.getElementById('overlay2').style.display = 'block';
+  document.getElementById('on2').style.display = 'none';
+  document.getElementById('off2').style.display = 'block';
+}
+
+function off2() {
+  document.getElementById('overlay2').style.display = 'none';
+  document.getElementById('on2').style.display = 'block';
+  document.getElementById('off2').style.display = 'none';
+}
+function on3() {
+  document.getElementById('overlay3').style.display = 'block';
+  document.getElementById('on3').style.display = 'none';
+  document.getElementById('off3').style.display = 'block';
+}
+
+function off3() {
+  document.getElementById('overlay3').style.display = 'none';
+  document.getElementById('on3').style.display = 'block';
+  document.getElementById('off3').style.display = 'none';
+}
