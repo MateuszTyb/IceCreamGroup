16261acd245e5378bdc930929209798b1c610124
MateuszTyb <86377061+MateuszTyb@users.noreply.github.com>
2021-09-17 00:51:41 +0200

    Działający modal

--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,13 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open] , [data-modal-mobileopen]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.openModalBtn.forEach(button => button.addEventListener('click', () => toggleModal()));
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
