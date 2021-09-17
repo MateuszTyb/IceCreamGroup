eb5fc641c0c8684b989eab8fca6466b78c063baa
Mateusz Tybinkowski <86377061+MateuszTyb@users.noreply.github.com>
2021-09-17 17:08:26 +0200

    Test prod

--- src/modal.js
+++ src/modal.js
@@ -6,7 +6,6 @@
   };
 
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
   refs.openModalBtn.forEach(button => button.addEventListener('click', () => toggleModal()));
 
   function toggleModal() {
